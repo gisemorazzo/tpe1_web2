bf9d332fe79b8ccc6e737f898cd8258039519304
gisele <giigii.morazzo@gmail.com>
2022-11-14 21:31:58 -0300

    se actualizo la base de datos

--- database/db_tienda_natura.sql
+++ database/db_tienda_natura.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 17-10-2022 a las 00:49:09
+-- Tiempo de generación: 15-11-2022 a las 00:51:04
 -- Versión del servidor: 10.4.24-MariaDB
 -- Versión de PHP: 8.1.6
 
@@ -37,8 +37,7 @@ CREATE TABLE `categoria` (
 --
 
 INSERT INTO `categoria` (`id`, `nombre`) VALUES
-(10, 'perfumes masculinos'),
-(11, 'perfumes femeninos'),
+(11, 'Perfumes Femeninos'),
 (13, 'cremas corporales'),
 (15, 'maquillaje'),
 (16, 'cremas faciales'),
@@ -55,20 +54,23 @@ CREATE TABLE `producto` (
   `nombre` text NOT NULL,
   `precio` float NOT NULL,
   `descripcion` text NOT NULL,
-  `id_categoria_fk` int(11) NOT NULL
+  `id_categoria_fk` int(11) NOT NULL,
+  `en_oferta` tinyint(1) NOT NULL DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Volcado de datos para la tabla `producto`
 --
 
-INSERT INTO `producto` (`id`, `nombre`, `precio`, `descripcion`, `id_categoria_fk`) VALUES
-(24, 'ilia', 8475, 'Una fragancia enriquecida con un ingrediente natural inédito extraído de la Paramela\r\nCamino olfativo:\r\n\r\nfloral intenso jazmin vainilla pomelo. \r\n\r\n \r\n\r\nInstrucciones de uso:\r\n\r\nPara resaltar más el perfume, aplique en las muñecas, en el cuello, en el escote y por detrás de las oreja.\r\n\r\n \r\n\r\nA quién está dirigido:\r\n\r\nPara la mujer independiente, activa y libre.\r\n\r\n \r\n\r\nContenido:\r\n\r\n50 ml.', 11),
-(25, 'Ilía Secreto', 8545, 'Una fragancia enriquecida con Ishpink, ingrediente natural inédito de la Amazonia que provoca los sentidos y guarda los secretos de la fuerza femenina. Misterio, fuerza y ​​sofisticación. La cremosidad de las flores blancas, como el muguet, contrasta con notas dulces de haba tonka, un acorde de frutas rojas y un toque exclusivo de café arábigo.', 11),
-(26, 'Natura Ekos Frescor Pitanga Eau de Toilette', 3346, 'Una fragancia radiante y refrescante. El brillo de un día de verano, que combina la frescura de las notas cítricas y hojas verdes con la ligereza del muguet, toque picante de jengibre envueltos por los acordes frutales de buriti, un ingrediente natural de la biodiversidad brasileña. Enriquecido con óleo esencial 100% natural de pitanga', 11),
-(27, 'Essencial Oud Eau de Parfum Masculino', 7861, 'Essencial es poder ser. Para protagonistas de su propia vida que son capaces de transformar e inspirar. La grandeza de Oud, la madera más noble del mundo, con la sensualidad de copaíba, una madera de la biodiversidad brasileña, realzada con un toque exótico de especias. Provocar al mismo tiempo que conquistar.', 10),
-(28, 'Crema Nutritiva para el Cuerpo Tododia Frambuesa y Pimienta Rosa', 1980, 'Despiertá tus sentidos con el Crema Nutritiva para el Cuerpo Tododia Frambuesa y Pimienta Rosa. Fórmula inteligente con nutrición prebiótica que se adapta a las necesidades diarias de tu piel. Tododia es día de sentirte bien con las cremas corporales de Natura Tododia. Tododia es día para ti.', 13),
-(29, 'Pulpa hidratante corporal Tukuma', 2145, 'Potente antiseñales corporal que estimula y protege la producción natural de ácido hialurónico. Con doble acción de manteca y óleo bruto de tukumá, rellena hasta las capas más profundas de la piel, actuando sobre los signos del envejecimiento. Piel renovada y uniforme.     Bioactivo Tukumá.  Estimula y protege la producción natural de ácido hialurónico de la piel.  Piel renovada y uniforme Textura cremosa Producto vegano Amazonia viva', 13);
+INSERT INTO `producto` (`id`, `nombre`, `precio`, `descripcion`, `id_categoria_fk`, `en_oferta`) VALUES
+(24, 'ilia', 8475, 'Una fragancia enriquecida con un ingrediente natural inédito extraído de la Paramela\r\nCamino olfativo:\r\n\r\nfloral intenso jazmin vainilla pomelo. \r\n\r\n \r\n\r\nInstrucciones de uso:\r\n\r\nPara resaltar más el perfume, aplique en las muñecas, en el cuello, en el escote y por detrás de las oreja.\r\n\r\n \r\n\r\nA quién está dirigido:\r\n\r\nPara la mujer independiente, activa y libre.\r\n\r\n \r\n\r\nContenido:\r\n\r\n50 ml.', 11, 0),
+(25, 'Ilía Secreto', 8545, 'Una fragancia enriquecida con Ishpink, ingrediente natural inédito de la Amazonia que provoca los sentidos y guarda los secretos de la fuerza femenina. Misterio, fuerza y ​​sofisticación. La cremosidad de las flores blancas, como el muguet, contrasta con notas dulces de haba tonka, un acorde de frutas rojas y un toque exclusivo de café arábigo.', 11, 0),
+(26, 'Natura Ekos Frescor Pitanga Eau de Toilette', 3346, 'Una fragancia radiante y refrescante. El brillo de un día de verano, que combina la frescura de las notas cítricas y hojas verdes con la ligereza del muguet, toque picante de jengibre envueltos por los acordes frutales de buriti, un ingrediente natural de la biodiversidad brasileña. Enriquecido con óleo esencial 100% natural de pitanga', 11, 0),
+(28, 'Crema Nutritiva para el Cuerpo Tododia Frambuesa y Pimienta Rosa', 1980, 'Despiertá tus sentidos con el Crema Nutritiva para el Cuerpo Tododia Frambuesa y Pimienta Rosa. Fórmula inteligente con nutrición prebiótica que se adapta a las necesidades diarias de tu piel. Tododia es día de sentirte bien con las cremas corporales de Natura Tododia. Tododia es día para ti.', 13, 0),
+(31, 'Rose', 8526, 'sarasa.', 11, 0),
+(32, 'dgbd', 8978, 'lorem jgjfdhg', 11, 0),
+(33, 'dshsdhh', 345, 'gsdg sdgsdgddsg', 11, 0),
+(34, 'test product', 7457, 'asgasf ewtwe', 11, 0);
 
 -- --------------------------------------------------------
 
@@ -90,7 +92,8 @@ CREATE TABLE `usuario` (
 INSERT INTO `usuario` (`id`, `email`, `nombre`, `password`) VALUES
 (1, 'giigii.morazzo@gmail.com', 'ivana gisele', '$2y$10$0dsT9xqh9/9MYfzAAcy29uyojuqKnG34rgWuY5TmxxjW7M9oJKZJe'),
 (2, 'ivana@gmail.com', 'gisele', '$2y$10$Dwul9QlsCcmPBZyWLo0Cy.m1RXyB8xiX5YIBK7R1KAwiFiWeU/tie'),
-(3, 'ivana.morazzo@hotmail.com', 'ivana gisele', '$2y$10$KaAUNK3gQfbFRO/zTF45z.OMC2gzeBHgaxi6mlI.rBACpN5XlZI7C');
+(3, 'ivana.morazzo@hotmail.com', 'ivana gisele', '$2y$10$KaAUNK3gQfbFRO/zTF45z.OMC2gzeBHgaxi6mlI.rBACpN5XlZI7C'),
+(4, 'hector@hotmail.com', 'hector lopez', '$2y$10$JXg9lZBE9awUF9gjCNfgO.Xd8TUgTWJoj9CiQWMCc2niMo0rOZWea');
 
 --
 -- Índices para tablas volcadas
@@ -130,13 +133,13 @@ ALTER TABLE `categoria`
 -- AUTO_INCREMENT de la tabla `producto`
 --
 ALTER TABLE `producto`
-  MODIFY `id` int(250) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30;
+  MODIFY `id` int(250) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=35;
 
 --
 -- AUTO_INCREMENT de la tabla `usuario`
 --
 ALTER TABLE `usuario`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- Restricciones para tablas volcadas
